fdc7598691a68ae870cfb6be43e777185554acb5
Kwabena Boateng Konadu <officiallykbk@gmail.com>
2025-11-12 13:44:14 +0000

    feat: add compose project

---
+++ compose/dockerPractice/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const app = express();
+const port = 3000;
+const prisma = new PrismaClient()
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.get('/', async (req: Request, res: Response) => {
+    const user = await prisma.user.findMany()
+    res.send(`Users are: ${user}`);
+
+    console.log('Welcome to the home On Server 1');
+
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
